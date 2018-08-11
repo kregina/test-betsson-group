12da6c6ddf5e7b7d63fb2ecb6d2e56ebb2a83801
Kelly Silva <kelly@kellyregina.com.br>
2018-08-11 20:17:33 -0300

    add ngx-hm-carousel

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
+import { NgxHmCarouselModule } from 'ngx-hm-carousel';
 
 @NgModule({
   imports: [
     CommonModule,
-    MaterialModule
+    MaterialModule,
+    NgxHmCarouselModule
   ],
   declarations: [],
   exports: [
-    MaterialModule
+    MaterialModule,
+    NgxHmCarouselModule
   ]
 })
 export class SharedModule { }
