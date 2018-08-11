648558bd0583f0ebfcde1b17ddd1cbb6636cb798
Kelly Silva <kelly@kellyregina.com.br>
2018-08-11 20:52:24 -0300

    add movies carousel

---
+++ src/app/movies-carousel/movies-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-carousel',
+  templateUrl: './movies-carousel.component.html',
+  styleUrls: ['./movies-carousel.component.scss']
+})
+export class MoviesCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
