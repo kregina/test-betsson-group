ce7333adf5717924948f250383c97cdb7e39a3e5
Kelly Silva <kelly@kellyregina.com.br>
2018-08-10 21:48:25 -0300

    animations refactor

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,29 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition
-} from '@angular/animations';
+import { fade, spin } from '../shared/animations/animations';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
-  animations: [
-    trigger('searchAnimation', [
-      state('show', style({
-        opacity: 1
-      })),
-      state('hide', style({
-        opacity: 0
-      })),
-      transition('show => hide', animate('200ms ease-out')),
-      transition('hide => show', animate('200ms ease-in'))
-    ])
-  ]
+  animations: [fade (), spin ()]
 })
 export class SearchComponent implements OnInit {
   showSearchInput = false;
---
+++ src/app/shared/animations/animations.ts
@@ -0,0 +1,27 @@
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  keyframes
+} from '@angular/animations';
+
+export const fade = (duration: number = 200) =>
+  trigger('fade', [
+    state('show', style({ opacity: 1 })),
+    state('hide', style({ opacity: 0 })),
+    transition('show => hide', animate(`${duration}ms ease-in`)),
+    transition('hide => show', animate(`${duration}ms ease-out`))
+  ]);
+
+export const spin = (duration: number = 200) =>
+  trigger('spin', [
+    transition('* => show', [
+      animate(`${duration}ms`, keyframes([
+        style({ transform: 'rotate(0)' }),
+        style({ transform: 'rotate(90deg)' })
+      ]))
+    ])
+  ]);
