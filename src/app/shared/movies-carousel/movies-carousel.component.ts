9b394e6ed575552711842ab0080a337d1c419f74
Kelly Silva <kelly@kellyregina.com.br>
2018-08-20 19:11:09 -0300

    fix mobile carousel

--- src/app/shared/movies-carousel/movies-carousel.component.ts
+++ src/app/shared/movies-carousel/movies-carousel.component.ts
@@ -1,20 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { PagedMovies } from '@app/core/themoviedb';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-movies-carousel',
   templateUrl: './movies-carousel.component.html',
   styleUrls: ['./movies-carousel.component.scss']
 })
-export class MoviesCarouselComponent {
-
+export class MoviesCarouselComponent implements OnDestroy {
+  private _mobileQueryListener: () => void;
+  mobileQuery: MediaQueryList;
   @Input() title: string;
   @Input() source: PagedMovies;
+  sliderNumber;
 
-  constructor() { }
+  constructor(private changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => this.changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+    this.sliderNumber = this.mobileQuery.matches ? 2 : 4;
+  }
 
-  change($event) {
-    console.log($event);
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
   }
 
 }
